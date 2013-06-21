5bfeba1dc8841d77c3c728a93d7ca8d3577fac46
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2013-06-21 11:37:25 +0200

    better formatting
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def headers():
-    return str(request.headers) 
+    return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
 
 @app.route("/favicon.ico")
 def favicon():
