6a20b2c1a2ae53af5aaa30df696844b4c834a7ee
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2013-06-21 11:00:59 +0200

    fix
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
-# -*- coding:utf-8 -*-                                
+# -*- coding:utf-8 -*-
 
-from flask import Flask, Response                     
-app = Flask(__name__)                                 
+from flask import Flask, Response
+app = Flask(__name__)
 
-@app.route("/")                                       
-def hello():                                          
+@app.route("/")
+def hello():
     return ('<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()]))
 
-@app.route("/favicon.ico")                            
-def favicon():                                        
-    resp = Response(status=200, mimetype='image/png') 
-    return resp                                       
+@app.route("/favicon.ico")
+def favicon():
+    resp = Response(status=200, mimetype='image/png')
+    return resp
 
-if __name__ == "__main__":                            
-    app.run()                                         
+if __name__ == "__main__":
+    app.run()
